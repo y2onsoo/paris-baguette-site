4d42a9cdc1972bcf86540bcda2d0caa1c0a0eda2
y2onsoo <abbcaog000@naver.com>
2023-04-27 07:13:30 +0900

    topbutton 추가

--- js/script.js
+++ js/script.js
@@ -27,3 +27,23 @@ $(".gnb li").mouseenter(function(){
 $(".nav-bg").mouseleave(function(){
   $("ul.sub,.nav-bg").hide();
 });
+
+
+
+//
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
